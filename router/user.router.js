5aabdf96bb0285b26432aec9a2f837d7dc441392
Abraham <abraham.kone@fyle-group.co>
2025-03-18 11:44:45 +0000

    Middleware error et fonction d'authentification

---
+++ router/user.router.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const CONTROLLER = require("../controllers/User.controller");
+
+router.post("/register", CONTROLLER.signup);
+router.post("/sign", CONTROLLER.signin);
+router.get("/all", CONTROLLER.getAllUsers);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,8 +6,32 @@ const { db } = require("./models");
 
 const app = express();
 
+// IMPORTATIONS DES ROUTES
+const userRouter = require("./router/user.router");
+
 // PORT
 const PORT = ENV.PORT || 8085;
+
+// MIDDLEWARE
+app.use(express.json());
+
+// PREFIX
+app.use("/api/user", userRouter);
+
+// MIDDLEWARE DE GESTION D'ERREURS
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Une erreur est survenue.";
+  const details = err.details || null;
+
+  res.status(status).json({
+    error: {
+      status,
+      message,
+      details,
+    },
+  });
+});
 
 // SERVEUR
 const startServer = async () => {
